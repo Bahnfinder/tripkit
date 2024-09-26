b88c24432c90e09b93fe3c6afc2805a8bcb84cf5
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2024-09-27 00:57:52 +0200

    Update VbbProvider.swift

--- Sources/TripKit/Provider/Implementations/VbbProvider.swift
+++ Sources/TripKit/Provider/Implementations/VbbProvider.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Verkehrsverbund Berlin-Brandenburg (DE)
 public class VbbProvider: AbstractHafasClientInterfaceProvider {
     
-    static let API_BASE = "https://fahrinfo.vbb.de/restproxy/2.32/"
+    static let API_BASE = "https://fahrinfo.vbb.de/bin/mgate.exe"
     static let PRODUCTS_MAP: [Product?] = [.suburbanTrain, .subway, .tram, .bus, .ferry, .highSpeedTrain, .regionalTrain, .onDemand, nil, nil]
 
     public override var supportedLanguages: Set<String> { ["de", "en"] }
